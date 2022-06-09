90fcd30072df26a0c523b13f7d9930c306f76e3a
Ramon van Raaij <ramon.van.raaij@gmail.com>
2022-06-09 15:09:24 +0200

    initial commit

---
+++ apps/forum-backend/jest.config.ts
@@ -0,0 +1,16 @@
+/* eslint-disable */
+export default {
+  displayName: 'forum-backend',
+  preset: '../../jest.preset.js',
+  globals: {
+    'ts-jest': {
+      tsconfig: '<rootDir>/tsconfig.spec.json',
+    },
+  },
+  testEnvironment: 'node',
+  transform: {
+    '^.+\\.[tj]s$': 'ts-jest',
+  },
+  moduleFileExtensions: ['ts', 'js', 'html'],
+  coverageDirectory: '../../coverage/apps/forum-backend',
+};
